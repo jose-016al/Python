c55a0214ab88068e54398b681a20358f92418a1d
Jose <81536276+jose-016al@users.noreply.github.com>
2022-03-29 01:27:27 +0200

    Delete Ejercicio3.py

--- Ejercicio3.py
+++
@@ -1,5 +0,0 @@
-# introducimos una cantidad de libras y las pasamos a euros 
-cantidad_libras = float(input("Introduce la cantidad de libras "))
-conversion = float(input("Introduce la conversion euro libra "))
-
-print("La cantidad en libras es: {}".format(cantidad_libras * conversion))
