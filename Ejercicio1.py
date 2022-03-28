bfe1cc7c8174f087b2e78713554185e9199102ea
Jose <81536276+jose-016al@users.noreply.github.com>
2022-03-29 01:27:07 +0200

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,8 +0,0 @@
-# Introducimos tres valores y nos indica cual es el mayor y el menor
-numero1 = int(input("Introduce el primer numero "))
-numero2 = int(input("Introduce el segundo numero "))
-numero3 = int(input("Introduce el tercero numero "))
-
-print("El numero mas grande entre {}, {} y {} es: {} y el mas pequeño es: {}".format(numero1, numero2, numero3,
-max(numero1 ,numero2, numero3),
-min(numero1, numero2, numero3)))
