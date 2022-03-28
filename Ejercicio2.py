e783aa0adcf36f15ff8080a5c3db39ede0131022
Jose <81536276+jose-016al@users.noreply.github.com>
2022-03-29 01:27:17 +0200

    Delete Ejercicio2.py

--- Ejercicio2.py
+++
@@ -1,5 +0,0 @@
-# introducimos un valor de grados fahrenheit y lo comvertimos a grados celsius
-grados_fahrenheit = int(input("¿ Grado Fahrenheit ? "))
-grados_celsius = (grados_fahrenheit - 32) * 5 / 9
-
-print("El resultado es: {}".format(grados_celsius))
